4ba713df567fefc5f62bf2d666e22f4788fc4eb6
Leah Redon <leah.redon@nurole.com>
2023-03-11 15:18:09 +0000

    Added data sample to interface

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.markdown("""# The Laundromat
+## Alicia Rémont Ospina, Claudia Jovellar, Tajania Kbabra, Leah Redon
+batch-2053-london""")
+
+df = pd.read_csv('/Users/leahredon/code/AlcaRmsp/the_laundromat/raw_data/data.csv')
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+# and used to select the displayed lines
+head_df = df.head(line_count)
+
+head_df
