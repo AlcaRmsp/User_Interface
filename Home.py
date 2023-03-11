6fad518f938b964e335f4bd6fa14fbf612e25bbe
Leah Redon <leah.redon@nurole.com>
2023-03-11 17:34:55 +0000

    Added animations to UI

--- Home.py
+++ Home.py
@@ -7,15 +7,17 @@ st.markdown("""# The Laundromat
 ## Alicia Rémont Ospina, Claudia Jovellar, Tajania Kbabra, Leah Redon
 batch-2053-london""")
 
-df = pd.read_csv('/Users/leahredon/code/AlcaRmsp/the_laundromat/raw_data/data.csv')
-df_sample = df[1:10]
 
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
 
-# and used to select the displayed lines
-head_df = df_sample.head(line_count)
+# df = pd.read_csv('/Users/leahredon/code/AlcaRmsp/the_laundromat/raw_data/data.csv')
+# df_sample = df[1:10]
 
-head_df
+# # this slider allows the user to select a number of lines
+# # to display in the dataframe
+# # the selected value is returned by st.slider
+# line_count = st.slider('Select a line count', 1, 10, 3)
+
+# # and used to select the displayed lines
+# head_df = df_sample.head(line_count)
+
+# head_df
---
+++ data/data_sample.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('/Users/leahredon/code/AlcaRmsp/the_laundromat/raw_data/data.csv')
+df_sample = df[1:10]
